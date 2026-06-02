c4a12364b8af8eb28f74078db7ebb1252833bc32
david15monsalvef-ctrl <david15monsalvef@gmail.com>
2026-06-02 16:56:45 -0500

    Delete models directory

--- models/Author.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const authorSchema = new Schema({
-  nombre: { type: String, required: true },
-  nacionalidad: { type: String, required: true },
-  fechaNacimiento: { type: Date, required: true }
-}, { timestamps: true });
-
-export default model('Author', authorSchema);
